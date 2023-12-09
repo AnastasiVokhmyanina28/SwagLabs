de9a77906425c4a86fe64f02af628d01307fe39f
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-12-09 12:31:02 +0300

    описание элементов авторизации

---
+++ src/main/java/PageObject/Elements/AuthorizationElements.java
@@ -0,0 +1,12 @@
+package PageObject.Elements;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class AuthorizationElements {
+    private static final SelenideElement userName = $("#user-name").as("Поле ввода 'UserName'");
+    private static final SelenideElement password = $("#password").as("Поле ввода 'Password'");
+    private static final SelenideElement logginButton = $("#login-button").as("Кнопка 'Login'");
+    private static final SelenideElement title = $(".login_logo").as("Заголовок");
+}
