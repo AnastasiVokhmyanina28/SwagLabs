459ac38f648fc2ede0c8b15046d52c3049328bc3
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-12-11 17:49:05 +0300

    описание элементов страниц

---
+++ src/main/java/PageObject/Elements/HomeElements.java
@@ -0,0 +1,10 @@
+package PageObject.Elements;
+
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+public class HomeElements {
+    private static final ElementsCollection cards = $$(".inventory_item").as("Карточки товаров на главное странице");
+}
