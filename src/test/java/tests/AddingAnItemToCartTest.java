7034474169314ce4b9bdca030eae600b9861a96e
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-12-13 17:19:41 +0300

    fix

--- src/test/java/tests/AddingAnItemToCartTest.java
+++ src/test/java/tests/AddingAnItemToCartTest.java
@@ -16,6 +16,7 @@ public class AddingAnItemToCartTest extends BaseClass {
     private HomePage homePage = new HomePage();
     private BaseStep baseStep = new BaseStep();
     private String price;
+    private CardsGoodsInTheCartElements container;
     private CardsGoodsInTheCartElements cardsGoodsInTheCartElements = new CardsGoodsInTheCartElements();
 
     @Test(description = "Добавление товара в корзину с главной страницы", dataProvider = "authParamUser", dataProviderClass = AuthorizationElements.class)
@@ -24,7 +25,7 @@ public class AddingAnItemToCartTest extends BaseClass {
         baseStep.emptyYourCart();
         homePage.homeElements.addItemToCart();
         price = homePage.homeElements.getPrice().getText();
-        CardsGoodsInTheCartElements container = homePage.toolBarElements.openContainer();
+        container = homePage.toolBarElements.openContainer();
         assertThat(container.getCards().isEmpty()).as("Корзина товаров пуста").isFalse();
         assertThat(cardsGoodsInTheCartElements.getProductName().getText()).as("Название товара отличается").isEqualTo(homePage.homeElements.getProductName().getText());
         assertThat(cardsGoodsInTheCartElements.getPrice().getText()).as("Стоимость товара отличается").isEqualTo(price);
