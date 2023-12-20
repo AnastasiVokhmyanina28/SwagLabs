80671882bfef459923a92e0a2f7d23d419eb911f
Анастасия Вохмянина <55682455+AnastasiVokhmyanina28@users.noreply.github.com>
2023-12-20 17:16:09 +0300

    code changes

--- src/main/java/PageObject/Elements/MainPage/HomePage.java
+++ src/main/java/PageObject/Elements/MainPage/HomePage.java
@@ -1,12 +1,8 @@
 package PageObject.Elements.MainPage;
 
-import static com.codeborne.selenide.Selenide.*;
-
+import Data.models.ProductPojo;
 import ToolBar.ToolBarElements;
 import com.codeborne.selenide.ElementsCollection;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import lombok.Getter;
@@ -15,11 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Getter
 public class HomePage implements ToolBarElements {
 
-    List<ProductBox> allProducts = initProducts();
-
+    public final SelenideElement allDeleteButton = $x("//button[contains(@id, 'remove')]").as("Все кнопки удаления");
+    public final ElementsCollection allButtons = $$x("//div[@class='inventory_item']//button").as("Кнопки из карточки товара");
     private final ElementsCollection productTableElements = $$(".inventory_item").as("Карточки товаров на главное странице");
     private final SelenideElement productSort = $(".product_sort_container").as("Сортировка товара");
     private final ElementsCollection listOfElementNames = $$(".inventory_item_name").as("Список наименований товаров");
@@ -27,27 +26,31 @@ public class HomePage implements ToolBarElements {
     /****/
     private final SelenideElement sortAZ = $x("//option[@value='az']").as("Сортировка по алфавиту");
     private final SelenideElement ascendingOrderOfPrice = $x("//option[@value='lohi']").as("Сортировка по возрастанию цены");
-
-
     private final SelenideElement productName = $("#item_4_title_link").as("Название товара");
     private final SelenideElement deleteButton = $("#remove-sauce-labs-backpack").as("Кнопка удаления товара");
-    public final SelenideElement allDeleteButton = $x("//button[contains(@id, 'remove')]").as("Все кнопки удаления");
-
-
-    public final ElementsCollection allButtons = $$x("//div[@class='inventory_item']//button").as("Кнопки из карточки товара");
     private final SelenideElement addButton = $("#add-to-cart-sauce-labs-backpack").as("Кнопка добавления товара");
     private final SelenideElement price = $x("//div[@class='inventory_item_label']/a[@id='item_4_title_link']/../following-sibling::div//div[@class='inventory_item_price']").as("Цена товара");
+    List<ProductBox> allProducts = initProducts();
 
     //todo
     private List<ProductBox> initProducts() {
         List<ProductBox> result = new ArrayList<>();
-        if (productTableElements != null) {
+        if (!productTableElements.isEmpty()) {
             productTableElements.forEach(
                     (element ->
                             result.add(new ProductBox(element))
                     )
             );
         }
+        return result;
+    }
+
+
+    public List<ProductPojo> getProducts() {
+        List result = new ArrayList<ProductPojo>();
+        allProducts.forEach(
+                productBox -> result.add(productBox.toPojo())
+        );
         return result;
     }
 
--- src/main/java/PageObject/Elements/MainPage/ProductBox.java
+++ src/main/java/PageObject/Elements/MainPage/ProductBox.java
@@ -3,25 +3,27 @@ package PageObject.Elements.MainPage;
 import Data.models.ProductPojo;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import lombok.NonNull;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import lombok.NonNull;
 
 
 public class ProductBox {
 
     SelenideElement container;
+    private SelenideElement itemName;
+    private SelenideElement itemPrice;
+    private SelenideElement addButton;
+    private SelenideElement deleteButton;
 
     public ProductBox(@NonNull SelenideElement container) {
         this.container = container;
+//todo
+        itemName = container.$(".inventory_item_name");
+        itemPrice = container.$(".inventory_item_price");
+        addButton = container.$x("//button[@class='btn btn_primary btn_small btn_inventory ']");
+        deleteButton = container.$x("//button[@class='btn btn_secondary btn_small btn_inventory ']");
     }
-
-    SelenideElement itemName = container.$(".inventory_item_name");
-    SelenideElement itemPrice = container.$(".inventory_item_price");
-    SelenideElement addButton = container.$x("//button[@class='btn btn_primary btn_small btn_inventory ']");
-    SelenideElement deleteButton = container.$x("//button[@class='btn btn_secondary btn_small btn_inventory ']");
-
 
     @Step
     public String getName() {
@@ -40,9 +42,7 @@ public class ProductBox {
 
     @Step
     public Boolean inCart() {
-        if (deleteButton.isDisplayed()) {
-            return true;
-        } else return false;
+        return deleteButton.isDisplayed();
     }
 
     @Step
